03d3845dc8d3636f24b59965b423ddeda6f14b13
jeremy-mazzocco <jeremyozzg@gmail.com>
2023-05-31 15:08:05 +0100

    add layout

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,40 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
+  <!-- <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+                <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+              </a>
+            </div>
+            <HelloWorld msg="Vite + Vue" /> -->
+  <header>
+    <div class="container"></div>
+  </header>
+
+  <main>
+    <div class="text">
+      <div class="container"></div>
+    </div>
+    <div class="jumbotron">
+      <div class="container"></div>
+    </div>
+    <div class="site-map">
+      <div class="container"></div>
+    </div>
+  </main>
+
+  <footer>
+    <div class="container"></div>
+  </footer>
 </template>
 
 <style scoped>
-.logo {
+/* .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
@@ -26,5 +45,5 @@ import HelloWorld from './components/HelloWorld.vue'
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
-}
+} */
 </style>
